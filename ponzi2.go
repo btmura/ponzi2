32aedaa0ac16918d20c1f8eb7396be35a7dfcee8
Brian Muramatsu <btmura@gmail.com>
2016-12-01 21:57:37 -0800

    copy glfw 3.2 sample

---
+++ ponzi2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
