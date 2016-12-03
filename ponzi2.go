39d6d4435d186b7fc5ccdb05359ce05ccc1fb8f5
Brian Muramatsu <btmura@gmail.com>
2016-12-03 11:02:05 -0800

    add go-bindata flag to strip prefix in gen code

--- ponzi2.go
+++ ponzi2.go
@@ -4,7 +4,7 @@ import "github.com/btmura/ponzi2/internal/ponzi"
 
 // This generate command is here, so we can just do "go generate" in the root directory.
 // Get go-bindata from github.com/jteeuwen/go-bindata. It's used to bundle resources.
-//go:generate go-bindata -pkg ponzi -o internal/ponzi/bindata.go internal/ponzi/data
+//go:generate go-bindata -pkg ponzi -o internal/ponzi/bindata.go -prefix internal/ponzi/data internal/ponzi/data
 
 func main() {
 	ponzi.Run()
