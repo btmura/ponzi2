119cfd545dc1e94c071a6ddc8eddd88290800478
Brian Muramatsu <btmura@gmail.com>
2016-12-02 22:05:05 -0800

    move glfw sample to internal package

---
+++ internal/ponzi/ponzi.go
@@ -0,0 +1,35 @@
+package ponzi
+
+import (
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+// Run runs the stock chart viewer in a window.
+func Run() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
