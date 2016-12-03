fa5c5e46ac81deeba9adab8ff7cb815b952c8e80
Brian Muramatsu <btmura@gmail.com>
2016-12-03 10:54:59 -0800

    add go generate to run go-bindata for resources

--- ponzi2.go
+++ ponzi2.go
@@ -2,6 +2,10 @@ package main
 
 import "github.com/btmura/ponzi2/internal/ponzi"
 
+// This generate command is here, so we can just do "go generate" in the root directory.
+// Get go-bindata from github.com/jteeuwen/go-bindata. It's used to bundle resources.
+//go:generate go-bindata -pkg ponzi -o internal/ponzi/bindata.go internal/ponzi/data
+
 func main() {
 	ponzi.Run()
 }
