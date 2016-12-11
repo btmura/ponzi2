c47be1f9aece276b5a7efb333d7e2f2667c95236
Brian Muramatsu <btmura@gmail.com>
2016-12-10 20:23:27 -0800

    move go generate command to ponzi package

--- internal/ponzi/ponzi.go
+++ internal/ponzi/ponzi.go
@@ -6,6 +6,9 @@ import (
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
+// Get go-bindata from github.com/jteeuwen/go-bindata. It's used to embed resources into the binary.
+//go:generate go-bindata -pkg ponzi -prefix data -ignore ".*blend.*" data
+
 func init() {
 	// This is needed to arrange that main() runs on main thread.
 	// See documentation for functions that are only allowed to be called from the main thread.
--- ponzi2.go
+++ ponzi2.go
@@ -2,9 +2,7 @@ package main
 
 import "github.com/btmura/ponzi2/internal/ponzi"
 
-// This generate command is here, so we can just do "go generate" in the root directory.
-// Get go-bindata from github.com/jteeuwen/go-bindata. It's used to bundle resources.
-//go:generate go-bindata -pkg ponzi -o internal/ponzi/bindata.go -prefix internal/ponzi/data -ignore ".*blend.*" internal/ponzi/data
+//go:generate go generate github.com/btmura/ponzi2/internal/ponzi
 
 func main() {
 	ponzi.Run()
