df0e6364b40dedbede0f25182b55d27432eda221
Brian Muramatsu <btmura@gmail.com>
2016-12-25 12:22:55 -0800

    render candlestick boxes

--- internal/ponzi/chart.go
+++ internal/ponzi/chart.go
@@ -13,7 +13,7 @@ type chart struct {
 }
 
 func createChart(sessions []*modelTradingSession) *chart {
-	// Find the min and max price.
+	// Find the min and max prices for the y-axis.
 	min := float32(math.MaxFloat32)
 	max := float32(0)
 	for _, s := range sessions {
@@ -25,27 +25,63 @@ func createChart(sessions []*modelTradingSession) *chart {
 		}
 	}
 
-	ns := len(sessions)
-	ws := 2.0 / float32(ns) // -1 to 1 on X-axis
-	x := -1.0 + ws/2.0
-
+	// Calculate vertices and indices for the candlesticks.
 	var vertices []float32
 	var indices []uint16
 
+	stickWidth := 2.0 / float32(len(sessions)) // (-1 to 1) on X-axis
+	leftX := -1.0 + stickWidth*0.1
+	midX := -1.0 + stickWidth*0.5
+	rightX := -1.0 + stickWidth*0.9
+
+	calcY := func(value float32) float32 {
+		return 2*(value-min)/(max-min) - 1
+	}
+
 	for i, s := range sessions {
-		lp := (s.low - min) / (max - min)
-		hp := (s.high - min) / (max - min)
-		vertices = append(vertices, x, 2*lp-1, x, 2*hp-1)
+		// Figure out Y coordinates of the key levels.
+		lowY, highY, openY, closeY := calcY(s.low), calcY(s.high), calcY(s.open), calcY(s.close)
 
-		op := (s.open - min) / (max - min)
-		vertices = append(vertices, x-ws/2, 2*op-1, x, 2*op-1)
+		// Figure out the top and bottom of the candlestick.
+		topY, botY := openY, closeY
+		if openY < closeY {
+			topY, botY = closeY, openY
+		}
 
-		cp := (s.close - min) / (max - min)
-		vertices = append(vertices, x, 2*cp-1, x+ws/2, 2*cp-1)
+		// Add the vertices needed to create the candlestick.
+		vertices = append(vertices,
+			midX, highY, // 0
+			midX, topY, // 1
+			midX, lowY, // 2
+			midX, botY, // 3
+			leftX, topY, // 4 - Upper left of box
+			rightX, topY, // 5 - Upper right of box
+			leftX, botY, // 6 - Bottom left of box
+			rightX, botY, // 7 - Bottom right of box
+		)
 
-		indices = append(indices, uint16(i*6), uint16(i*6+1), uint16(i*6+2), uint16(i*6+3), uint16(i*6+4), uint16(i*6+5))
+		// idx is function to refer to the vertices above.
+		idx := func(j uint16) uint16 {
+			return uint16(i)*8 + j
+		}
 
-		x += ws
+		// Add the vertex indices to render the candlestick.
+		indices = append(indices,
+			// Top and bottom lines around the box.
+			idx(0), idx(1),
+			idx(2), idx(3),
+
+			// Lines for the box.
+			idx(4), idx(5),
+			idx(6), idx(7),
+			idx(4), idx(6),
+			idx(5), idx(7),
+		)
+
+		// Move the X coordinates one stick over.
+		leftX += stickWidth
+		midX += stickWidth
+		rightX += stickWidth
 	}
 
 	vbo := createArrayBuffer(vertices)
