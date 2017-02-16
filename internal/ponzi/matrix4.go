30f37d1b3b46d84f4cfb647e8bc9d1dba3451285
Brian Muramatsu <btmura@gmail.com>
2017-02-15 19:14:29 -0800

    fix lint warnings in matrix4.go

--- internal/ponzi/matrix4.go
+++ internal/ponzi/matrix4.go
@@ -173,35 +173,35 @@ func (m matrix4) inverse() matrix4 {
 	m31 := m[3*4+1]
 	m32 := m[3*4+2]
 	m33 := m[3*4+3]
-	tmp_0 := m22 * m33
-	tmp_1 := m32 * m23
-	tmp_2 := m12 * m33
-	tmp_3 := m32 * m13
-	tmp_4 := m12 * m23
-	tmp_5 := m22 * m13
-	tmp_6 := m02 * m33
-	tmp_7 := m32 * m03
-	tmp_8 := m02 * m23
-	tmp_9 := m22 * m03
-	tmp_10 := m02 * m13
-	tmp_11 := m12 * m03
-	tmp_12 := m20 * m31
-	tmp_13 := m30 * m21
-	tmp_14 := m10 * m31
-	tmp_15 := m30 * m11
-	tmp_16 := m10 * m21
-	tmp_17 := m20 * m11
-	tmp_18 := m00 * m31
-	tmp_19 := m30 * m01
-	tmp_20 := m00 * m21
-	tmp_21 := m20 * m01
-	tmp_22 := m00 * m11
-	tmp_23 := m10 * m01
+	tmp0 := m22 * m33
+	tmp1 := m32 * m23
+	tmp2 := m12 * m33
+	tmp3 := m32 * m13
+	tmp4 := m12 * m23
+	tmp5 := m22 * m13
+	tmp6 := m02 * m33
+	tmp7 := m32 * m03
+	tmp8 := m02 * m23
+	tmp9 := m22 * m03
+	tmp10 := m02 * m13
+	tmp11 := m12 * m03
+	tmp12 := m20 * m31
+	tmp13 := m30 * m21
+	tmp14 := m10 * m31
+	tmp15 := m30 * m11
+	tmp16 := m10 * m21
+	tmp17 := m20 * m11
+	tmp18 := m00 * m31
+	tmp19 := m30 * m01
+	tmp20 := m00 * m21
+	tmp21 := m20 * m01
+	tmp22 := m00 * m11
+	tmp23 := m10 * m01
 
-	t0 := (tmp_0*m11 + tmp_3*m21 + tmp_4*m31) - (tmp_1*m11 + tmp_2*m21 + tmp_5*m31)
-	t1 := (tmp_1*m01 + tmp_6*m21 + tmp_9*m31) - (tmp_0*m01 + tmp_7*m21 + tmp_8*m31)
-	t2 := (tmp_2*m01 + tmp_7*m11 + tmp_10*m31) - (tmp_3*m01 + tmp_6*m11 + tmp_11*m31)
-	t3 := (tmp_5*m01 + tmp_8*m11 + tmp_11*m21) - (tmp_4*m01 + tmp_9*m11 + tmp_10*m21)
+	t0 := (tmp0*m11 + tmp3*m21 + tmp4*m31) - (tmp1*m11 + tmp2*m21 + tmp5*m31)
+	t1 := (tmp1*m01 + tmp6*m21 + tmp9*m31) - (tmp0*m01 + tmp7*m21 + tmp8*m31)
+	t2 := (tmp2*m01 + tmp7*m11 + tmp10*m31) - (tmp3*m01 + tmp6*m11 + tmp11*m31)
+	t3 := (tmp5*m01 + tmp8*m11 + tmp11*m21) - (tmp4*m01 + tmp9*m11 + tmp10*m21)
 
 	d := 1.0 / (m00*t0 + m10*t1 + m20*t2 + m30*t3)
 
@@ -210,18 +210,18 @@ func (m matrix4) inverse() matrix4 {
 		d * t1,
 		d * t2,
 		d * t3,
-		d * ((tmp_1*m10 + tmp_2*m20 + tmp_5*m30) - (tmp_0*m10 + tmp_3*m20 + tmp_4*m30)),
-		d * ((tmp_0*m00 + tmp_7*m20 + tmp_8*m30) - (tmp_1*m00 + tmp_6*m20 + tmp_9*m30)),
-		d * ((tmp_3*m00 + tmp_6*m10 + tmp_11*m30) - (tmp_2*m00 + tmp_7*m10 + tmp_10*m30)),
-		d * ((tmp_4*m00 + tmp_9*m10 + tmp_10*m20) - (tmp_5*m00 + tmp_8*m10 + tmp_11*m20)),
-		d * ((tmp_12*m13 + tmp_15*m23 + tmp_16*m33) - (tmp_13*m13 + tmp_14*m23 + tmp_17*m33)),
-		d * ((tmp_13*m03 + tmp_18*m23 + tmp_21*m33) - (tmp_12*m03 + tmp_19*m23 + tmp_20*m33)),
-		d * ((tmp_14*m03 + tmp_19*m13 + tmp_22*m33) - (tmp_15*m03 + tmp_18*m13 + tmp_23*m33)),
-		d * ((tmp_17*m03 + tmp_20*m13 + tmp_23*m23) - (tmp_16*m03 + tmp_21*m13 + tmp_22*m23)),
-		d * ((tmp_14*m22 + tmp_17*m32 + tmp_13*m12) - (tmp_16*m32 + tmp_12*m12 + tmp_15*m22)),
-		d * ((tmp_20*m32 + tmp_12*m02 + tmp_19*m22) - (tmp_18*m22 + tmp_21*m32 + tmp_13*m02)),
-		d * ((tmp_18*m12 + tmp_23*m32 + tmp_15*m02) - (tmp_22*m32 + tmp_14*m02 + tmp_19*m12)),
-		d * ((tmp_22*m22 + tmp_16*m02 + tmp_21*m12) - (tmp_20*m12 + tmp_23*m22 + tmp_17*m02)),
+		d * ((tmp1*m10 + tmp2*m20 + tmp5*m30) - (tmp0*m10 + tmp3*m20 + tmp4*m30)),
+		d * ((tmp0*m00 + tmp7*m20 + tmp8*m30) - (tmp1*m00 + tmp6*m20 + tmp9*m30)),
+		d * ((tmp3*m00 + tmp6*m10 + tmp11*m30) - (tmp2*m00 + tmp7*m10 + tmp10*m30)),
+		d * ((tmp4*m00 + tmp9*m10 + tmp10*m20) - (tmp5*m00 + tmp8*m10 + tmp11*m20)),
+		d * ((tmp12*m13 + tmp15*m23 + tmp16*m33) - (tmp13*m13 + tmp14*m23 + tmp17*m33)),
+		d * ((tmp13*m03 + tmp18*m23 + tmp21*m33) - (tmp12*m03 + tmp19*m23 + tmp20*m33)),
+		d * ((tmp14*m03 + tmp19*m13 + tmp22*m33) - (tmp15*m03 + tmp18*m13 + tmp23*m33)),
+		d * ((tmp17*m03 + tmp20*m13 + tmp23*m23) - (tmp16*m03 + tmp21*m13 + tmp22*m23)),
+		d * ((tmp14*m22 + tmp17*m32 + tmp13*m12) - (tmp16*m32 + tmp12*m12 + tmp15*m22)),
+		d * ((tmp20*m32 + tmp12*m02 + tmp19*m22) - (tmp18*m22 + tmp21*m32 + tmp13*m02)),
+		d * ((tmp18*m12 + tmp23*m32 + tmp15*m02) - (tmp22*m32 + tmp14*m02 + tmp19*m12)),
+		d * ((tmp22*m22 + tmp16*m02 + tmp21*m12) - (tmp20*m12 + tmp23*m22 + tmp17*m02)),
 	}
 }
 
