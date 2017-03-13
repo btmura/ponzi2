2c94d78e76fc0f3783c9c3aebf7c0ba55931e735
Brian Muramatsu <btmura@gmail.com>
2017-03-12 23:00:12 -0700

    render line when week changes

--- internal/ponzi/chartline.go
+++ internal/ponzi/chartline.go
@@ -26,27 +26,34 @@ func (ch *chartLines) update() {
 	var colors []float32
 	var indices []uint16
 
+	// Amount to move on X-axis for one session.
 	dx := 2.0 / float32(len(ch.stock.dailySessions))
-	calcX := func(i int) float32 {
-		return -1 + dx*float32(i)
-	}
 
+	// Render lines whenever the week number changes.
 	for i, s := range ch.stock.dailySessions {
-		if i > 0 && ch.stock.dailySessions[i-1].date.Month() != s.date.Month() {
-			x := calcX(i)
-			vertices = append(vertices,
-				x, -1,
-				x, +1,
-			)
-			colors = append(colors,
-				gray[0], gray[1], gray[2],
-				gray[0], gray[1], gray[2],
-			)
-			indices = append(indices,
-				uint16(len(vertices)-1),
-				uint16(len(vertices)-2),
-			)
+		if i == 0 {
+			continue // Can't check previous week.
 		}
+
+		_, pwk := ch.stock.dailySessions[i-1].date.ISOWeek()
+		_, wk := s.date.ISOWeek()
+		if pwk == wk {
+			continue
+		}
+
+		x := -1 + dx*float32(i)
+		vertices = append(vertices,
+			x, -1,
+			x, +1,
+		)
+		colors = append(colors,
+			gray[0], gray[1], gray[2],
+			gray[0], gray[1], gray[2],
+		)
+		indices = append(indices,
+			uint16(len(vertices)-1),
+			uint16(len(vertices)-2),
+		)
 	}
 
 	ch.monthLines = createVAO(gl.LINES, vertices, colors, indices)
