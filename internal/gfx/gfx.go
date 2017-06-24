10f19374d86ccfd08058a9d9a34a30619e97b866
Brian Muramatsu <btmura@gmail.com>
2017-06-23 22:33:13 -0700

    create experimental gfx package

---
+++ internal/gfx/gfx.go
@@ -0,0 +1,4 @@
+package gfx
+
+// Get go-bindata from github.com/jteeuwen/go-bindata. It's used to embed resources into the binary.
+//go:generate go-bindata -pkg gfx -prefix data -ignore ".*blend.*" data
--- internal/ponzi/ponzi.go
+++ internal/ponzi/ponzi.go
@@ -10,6 +10,8 @@ import (
 // Get go-bindata from github.com/jteeuwen/go-bindata. It's used to embed resources into the binary.
 //go:generate go-bindata -pkg ponzi -prefix data -ignore ".*blend.*" data
 
+//go:generate go generate github.com/btmura/ponzi2/internal/gfx
+
 func init() {
 	// This is needed to arrange that main() runs on main thread.
 	// See documentation for functions that are only allowed to be called from the main thread.
