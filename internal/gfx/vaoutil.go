122657937567701b0dc48c0fe563536b88f269ea
Brian Muramatsu <btmura@gmail.com>
2017-09-07 10:59:25 -0700

    add rounded corner border aroun chart

--- internal/gfx/vaoutil.go
+++ internal/gfx/vaoutil.go
@@ -31,6 +31,26 @@ func HorizColoredLineVAO(leftColor, rightColor VAOColor) *VAO {
 	)
 }
 
+// VertColoredLineVAO returns a horizontal colored line segment from (0, -1) to (0, 1).
+func VertColoredLineVAO(topColor, botColor VAOColor) *VAO {
+	return NewVAO(
+		Lines,
+		&VAOVertexData{
+			Vertices: []float32{
+				0, -1, 0,
+				0, +1, 0,
+			},
+			Colors: []float32{
+				topColor[0], topColor[1], topColor[2],
+				botColor[0], botColor[1], botColor[2],
+			},
+			Indices: []uint16{
+				0, 1,
+			},
+		},
+	)
+}
+
 // ReadPLYVAO returns a VAO decoded from PLY reader.
 func ReadPLYVAO(r io.Reader) *VAO {
 	p, err := ply.Decode(r)
@@ -38,40 +58,49 @@ func ReadPLYVAO(r io.Reader) *VAO {
 		glog.Fatalf("gfx.ReadPLYVAO: decoding PLY failed: %v", err)
 	}
 
-	var vertices []float32
-	var normals []float32
-	var texCoords []float32
-	var indices []uint16
+	data := &VAOVertexData{}
 
 	for _, e := range p.Elements["vertex"] {
 		x := e.Float32s["x"]
 		y := e.Float32s["y"]
 		z := e.Float32s["z"]
-		vertices = append(vertices, x, y, z)
+		data.Vertices = append(data.Vertices, x, y, z)
 
-		nx := e.Float32s["nx"]
-		ny := e.Float32s["ny"]
-		nz := e.Float32s["nz"]
-		normals = append(normals, nx, ny, nz)
+		if nx, ok := e.Float32s["nx"]; ok {
+			ny := e.Float32s["ny"]
+			nz := e.Float32s["nz"]
+			data.Normals = append(data.Normals, nx, ny, nz)
+		}
 
-		s := e.Float32s["s"]
-		t := e.Float32s["t"]
-		texCoords = append(texCoords, s, 1-t)
-	}
+		if s, ok := e.Float32s["s"]; ok {
+			t := e.Float32s["t"]
+			data.TexCoords = append(data.TexCoords, s, 1-t)
+		}
 
-	for _, e := range p.Elements["face"] {
-		for _, idx := range e.Uint32Lists["vertex_indices"] {
-			indices = append(indices, uint16(idx))
+		if r, ok := e.Uint8s["red"]; ok {
+			g := e.Uint8s["green"]
+			b := e.Uint8s["blue"]
+			data.Colors = append(data.Colors, float32(r)/0xff, float32(g)/0xff, float32(b)/0xff)
 		}
 	}
 
-	return NewVAO(
-		Triangles,
-		&VAOVertexData{
-			Vertices:  vertices,
-			Normals:   normals,
-			TexCoords: texCoords,
-			Indices:   indices,
-		},
-	)
+	// TODO(btmura): fail if its not a triangle
+	for _, e := range p.Elements["face"] {
+		for _, idx := range e.Uint32Lists["vertex_indices"] {
+			data.Indices = append(data.Indices, uint16(idx))
+		}
+	}
+
+	// TODO(btmura): support multiple drawings
+	if len(data.Indices) > 0 {
+		return NewVAO(Triangles, data)
+	}
+
+	for _, e := range p.Elements["edge"] {
+		v1 := e.Int32s["vertex1"]
+		v2 := e.Int32s["vertex2"]
+		data.Indices = append(data.Indices, uint16(v1), uint16(v2))
+	}
+
+	return NewVAO(Lines, data)
 }
