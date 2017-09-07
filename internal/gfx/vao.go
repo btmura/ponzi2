95338a04d3c2d207e91b3a051f77feb7e64e88cc
Brian Muramatsu <btmura@gmail.com>
2017-09-07 15:29:56 -0700

    defer actual vao creation until first rendering

--- internal/gfx/vao.go
+++ internal/gfx/vao.go
@@ -7,17 +7,19 @@ import (
 
 // VAO is a Vertex Array Object (VAO) for one element type (lines, triangles, etc).
 type VAO struct {
-	array uint32 // array is the VAO name for gl.BindVertexArray.
-	mode  uint32 // mode is like gl.LINES or gl.TRIANGLES passed to gl.DrawElements.
-	count int32  // count is the number of elements for gl.DrawElements.
+	initData *VAOVertexData // initData is used to initialize the VAO when needed. Nil after it is used.
+	array    uint32         // array is the VAO name for gl.BindVertexArray.
+	mode     uint32         // mode is like gl.LINES or gl.TRIANGLES passed to gl.DrawElements.
+	count    int32          // count is the number of elements for gl.DrawElements.
 }
 
 // VAODrawMode is analogous to the mode argument to gl.DrawElements like gl.LINES or gl.TRIANGLES.
 type VAODrawMode int
 
-// VAODrawMode enums.
+// Enum values for VAODrawMode.
 const (
-	Triangles VAODrawMode = iota
+	Unspecified VAODrawMode = iota
+	Triangles
 	Lines
 )
 
@@ -30,12 +32,48 @@ type VAOVertexData struct {
 	Indices   []uint16  // Indices is a required slice of indices into all the buffers.
 }
 
-// NewVAO creates a VAO out of the given data buffers and drawing mode.
+// NewVAO creates a VAO with the given data buffers and a drawing mode.
+//
+// Since NewVAO defers the creation of the actual OpenGL VAO till the first rendering,
+// callers may call NewVAO at the package scope to simplify their code.
 func NewVAO(mode VAODrawMode, data *VAOVertexData) *VAO {
-	if len(data.Vertices) == 0 || len(data.Indices) == 0 {
+	if len(data.Indices) == 0 {
 		return &VAO{} // OpenGL doesn't allow empty buffer objects. Return VAO with zero count.
 	}
 
+	// TODO(btmura): store draw mode instead of the GL value and use unspecified in Delete
+	var glMode uint32
+	switch mode {
+	case Triangles:
+		glMode = gl.TRIANGLES
+	case Lines:
+		glMode = gl.LINES
+	default:
+		glog.Fatalf("gfx.NewVAO: unsupported mode: %v", mode)
+	}
+
+	return &VAO{
+		initData: data,
+		mode:     glMode,
+	}
+}
+
+// Render renders the VAO.
+func (v *VAO) Render() {
+	if v.initData != nil {
+		v.array, v.count = createVAO(v.initData)
+		v.initData = nil // Reset initData to indicate the data was used to create the VAO.
+	}
+	if v.count == 0 {
+		return // No buffer data. Nothing to render.
+	}
+	gl.BindVertexArray(v.array)
+	gl.DrawElements(v.mode, v.count, gl.UNSIGNED_SHORT, gl.Ptr(nil))
+	gl.BindVertexArray(0)
+}
+
+// createVAO blindly creates the VAO using the given vertex data.
+func createVAO(data *VAOVertexData) (array uint32, count int32) {
 	vbo := arrayBuffer(data.Vertices)
 
 	var nbo uint32
@@ -54,8 +92,6 @@ func NewVAO(mode VAODrawMode, data *VAOVertexData) *VAO {
 	}
 
 	ibo := elementArrayBuffer(data.Indices)
-
-	var array uint32
 
 	gl.GenVertexArrays(1, &array)
 	gl.BindVertexArray(array)
@@ -86,35 +122,21 @@ func NewVAO(mode VAODrawMode, data *VAOVertexData) *VAO {
 	}
 	gl.BindVertexArray(0)
 
-	var glMode uint32
-	switch mode {
-	case Triangles:
-		glMode = gl.TRIANGLES
-	case Lines:
-		glMode = gl.LINES
-	default:
-		glog.Fatalf("gfx.NewVAO: unsupported mode: %v", mode)
-	}
-
-	return &VAO{
-		array: array,
-		mode:  glMode,
-		count: int32(len(data.Indices)),
-	}
-}
-
-// Render renders the VAO.
-func (v *VAO) Render() {
-	if v.count == 0 {
-		return // No buffer data. Nothing to render.
-	}
-	gl.BindVertexArray(v.array)
-	gl.DrawElements(v.mode, v.count, gl.UNSIGNED_SHORT, gl.Ptr(nil))
-	gl.BindVertexArray(0)
+	return array, int32(len(data.Indices))
 }
 
 // Delete deletes the VAO. Don't call Render after calling this.
 func (v *VAO) Delete() {
+	defer func() {
+		v.initData = nil
+		v.array = 0
+		v.mode = 0
+		v.count = 0
+	}()
+
+	if v.initData != nil {
+		return // Data never used to create the VAO. Bail out.
+	}
 	if v.count == 0 {
 		return // No buffer data. Nothing to delete.
 	}
