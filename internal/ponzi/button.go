af663069b4c1f781159b9c55068ab6ee45f9f58a
Brian Muramatsu <btmura@gmail.com>
2017-09-10 09:21:07 -0700

    add some comments for button.go

--- internal/ponzi/button.go
+++ internal/ponzi/button.go
@@ -4,17 +4,20 @@ import (
 	"github.com/btmura/ponzi2/internal/gfx"
 )
 
+// Button is a button that can be rendered and clicked.
 type Button struct {
 	iconVAO        *gfx.VAO
 	clickCallbacks []func()
 }
 
+// NewButton creates a new button.
 func NewButton(iconVAO *gfx.VAO) *Button {
 	return &Button{
 		iconVAO: iconVAO,
 	}
 }
 
+// Render renders the button and detects button clicks.
 func (b *Button) Render(vc ViewContext) {
 	if vc.LeftClickInBounds() {
 		vc.scheduleCallbacks(b.clickCallbacks)
@@ -25,6 +28,7 @@ func (b *Button) Render(vc ViewContext) {
 	b.iconVAO.Render()
 }
 
+// AddClickCallback adds a callback for when the button is clicked.
 func (b *Button) AddClickCallback(cb func()) {
 	b.clickCallbacks = append(b.clickCallbacks, cb)
 }
