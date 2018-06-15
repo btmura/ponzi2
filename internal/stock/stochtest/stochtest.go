47d8706378d1ce5808b2d192288bd792a543103d
Brian Muramatsu <btmura@gmail.com>
2018-06-15 08:53:03 -0700

    move cl program to stock package as a test app

---
+++ internal/stock/stochtest/stochtest.go
@@ -0,0 +1,26 @@
+// Binary stochktest exercises the stock.GetStochastics function.
+//
+// go run internal/stock/stochtest/stochtest.go
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/golang/glog"
+
+	"github.com/btmura/ponzi2/internal/stock"
+)
+
+var alphaVantageAPIKey = flag.String("alpha_vantage_api_key", "", "Alpha Vantage API Key")
+
+func main() {
+	flag.Parse()
+
+	av := stock.NewAlphaVantage(*alphaVantageAPIKey)
+	resp, err := av.GetStochastics(&stock.StochasticRequest{Symbol: "SPY"})
+	if err != nil {
+		glog.Exit(err)
+	}
+	fmt.Println(resp)
+}
