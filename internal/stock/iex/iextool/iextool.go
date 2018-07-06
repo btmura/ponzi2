124f7443b4f52998c5565e28ba7aaf0284fd33d0
Brian Muramatsu <btmura@gmail.com>
2018-07-05 20:31:24 -0700

    change iex example to standalone tool

---
+++ internal/stock/iex/iextool/iextool.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/btmura/ponzi2/internal/stock/iex"
+)
+
+var (
+	symbol           = flag.String("symbol", "SPY", "Symbol to lookup.")
+	dumpAPIResponses = flag.Bool("dump_api_responses", false, "Dump API responses to txt files.")
+)
+
+func main() {
+	ctx := context.Background()
+
+	c := iex.NewClient(*dumpAPIResponses)
+
+	req := &iex.GetTradingSessionSeriesRequest{Symbol: *symbol}
+	sr, err := c.GetTradingSessionSeries(ctx, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, ts := range sr.TradingSessions {
+		fmt.Printf("%d: %s O: %.2f H: %.2f L: %.2f C: %.2f V: %d\n",
+			i, ts.Date, ts.Open, ts.High, ts.Low, ts.Close, ts.Volume)
+	}
+}
