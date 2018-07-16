1d9574f2abd52987ad5bb17404d7ce1aea8feec1
Brian Muramatsu <btmura@gmail.com>
2018-07-15 17:48:53 -0700

    fix logger call depth

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,25 +2,31 @@
 package logger
 
 import (
+	"fmt"
+
 	"github.com/golang/glog"
 )
 
 // Info logs an Info message.
 func Info(args ...interface{}) {
-	glog.V(2).Info(args...)
+	if glog.V(2) {
+		glog.InfoDepth(1, args...)
+	}
 }
 
 // Infof logs an Info message
 func Infof(format string, args ...interface{}) {
-	glog.V(2).Infof(format, args...)
+	if glog.V(2) {
+		glog.InfoDepth(1, fmt.Sprintf(format, args...))
+	}
 }
 
 // Fatal logs and exits.
 func Fatal(args ...interface{}) {
-	glog.Fatal(args...)
+	glog.FatalDepth(1, args...)
 }
 
 // Fatalf logs and exits.
 func Fatalf(format string, args ...interface{}) {
-	glog.Fatalf(format, args...)
+	glog.FatalDepth(1, fmt.Sprintf(format, args...))
 }
