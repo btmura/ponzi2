122827b6a5c487e4e7406697854e0999da3b895e
Brian Muramatsu <btmura@gmail.com>
2019-07-11 19:34:40 -0700

    remove server from iextool, clean  up code

--- internal/stock/iex/iextool/iextool.go
+++ internal/stock/iex/iextool/iextool.go
@@ -3,11 +3,12 @@ package main
 
 import (
 	"context"
+	"expvar"
 	_ "expvar"
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/btmura/ponzi2/internal/stock/iex"
@@ -22,12 +23,12 @@ var (
 func main() {
 	flag.Parse()
 
-	fmt.Printf("Serving metrics at http://localhost:%d/debug/vars\n", *port)
-	go func() {
-		if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
-			fmt.Printf("http.ListenAndServe failed: %v\n", err)
-		}
-	}()
+	// fmt.Printf("Serving metrics at http://localhost:%d/debug/vars\n", *port)
+	// go func() {
+	// 	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+	// 		fmt.Printf("http.ListenAndServe failed: %v\n", err)
+	// 	}
+	// }()
 
 	ctx := context.Background()
 
@@ -43,26 +44,13 @@ func main() {
 		fmt.Scanln(&symbolLine)
 		symbolLine = strings.ToUpper(symbolLine)
 
-		rangePick := 1 // 0 is unspecified.
-		for {
-			fmt.Println("Pick a range:")
-			for i := 1; i < 3; i++ {
-				fmt.Printf("[%d] %v\n", i, iex.Range(i))
-			}
-
-			if _, err := fmt.Scanf("%d", &rangePick); err != nil {
-				fmt.Println(err)
-				continue
-			}
-
-			break
-		}
+		dataRange := pick("Pick a range", iex.OneDay, iex.TwoYears).(iex.Range)
 
 		qReq := &iex.GetQuotesRequest{
 			Symbols: strings.Split(symbolLine, ","),
 		}
 
-		fmt.Printf("Quotes: %+v\n", qReq)
+		fmt.Printf("Quotes: %+v\n\n", qReq)
 
 		quotes, err := c.GetQuotes(ctx, qReq)
 		if err != nil {
@@ -71,17 +59,19 @@ func main() {
 		}
 
 		for i, q := range quotes {
-			fmt.Printf("%d: %s Open: %.2f High: %.2f Low: %.2f Close: %.2f Volume: %d\n",
+			fmt.Printf("%d: %-4s O: %.2f H: %.2f L: %.2f C: %.2f V: %d\n",
 				i, q.Symbol, q.Open, q.High, q.Low, q.Close, q.LatestVolume)
 		}
 
+		fmt.Println()
+
 		cReq := &iex.GetChartsRequest{
 			Symbols:   strings.Split(symbolLine, ","),
-			Range:     iex.Range(rangePick),
+			Range:     dataRange,
 			ChartLast: 3,
 		}
 
-		fmt.Printf("Charts: %+v\n", cReq)
+		fmt.Printf("Charts: %+v\n\n", cReq)
 
 		charts, err := c.GetCharts(ctx, cReq)
 		if err != nil {
@@ -92,9 +82,54 @@ func main() {
 		for i, ch := range charts {
 			fmt.Printf("%d: %s\n", i, ch.Symbol)
 			for j, p := range ch.ChartPoints {
-				fmt.Printf("\t%d: %s Open: %.2f High: %.2f Low: %.2f Close: %.2f Volume: %d\n",
-					j, ch.Symbol, p.Open, p.High, p.Low, p.Close, p.Volume)
+				fmt.Printf("\t%d: D: %v O: %.2f H: %.2f L: %.2f C: %.2f V: %d\n",
+					j, p.Date.Format("1/2/06 03:04 AM"), p.Open, p.High, p.Low, p.Close, p.Volume)
 			}
 		}
+
+		fmt.Println()
+
+		expvar.Do(func(kv expvar.KeyValue) {
+			if !strings.HasPrefix(kv.Key, "iex-") {
+				return
+			}
+			fmt.Println(kv)
+		})
+
+		fmt.Println()
 	}
+}
+
+func pick(prompt string, choices ...interface{}) interface{} {
+	selected := 0
+	for {
+		fmt.Printf("%s [%d]:\n", prompt, selected)
+		for i, v := range choices {
+			fmt.Printf("[%d] %v\n", i, v)
+		}
+
+		line := ""
+		fmt.Scanln(&line)
+
+		line = strings.TrimSpace(line)
+		if line == "" {
+			break
+		}
+
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if i < 0 || i >= len(choices) {
+			fmt.Printf("Enter a value from %d to %d\n", 0, len(choices)-1)
+			continue
+		}
+
+		selected = i
+		break
+	}
+
+	return choices[selected]
 }
